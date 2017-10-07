6f7d98ca8f5c57c1d09194a9a75396939d4991f5
picasso <picasso@picasso-kubuntu>
2017-10-08 00:10:17 +0300

    fix

--- spinner/spinner/cmd/sync.py
+++ spinner/spinner/cmd/sync.py
@@ -18,8 +18,8 @@ from spinner.local_settings import SERVERS
 
 logging.basicConfig(filename=os.path.join(SITE_ROOT, 'xmlrpc.log'),level=logging.INFO, format='%(asctime)s %(message)s')
 
-def get_context(srvr):        
-    if srvr.get('ssl') == True:    
+def get_context(srvr):
+    if srvr.get('ssl') == True:
         context = ssl.create_default_context(cafile=srvr['cafile'])
         context.verify_mode = ssl.CERT_REQUIRED
         context.load_cert_chain(srvr['pemfile'], keyfile=srvr['keyfile'])
@@ -27,7 +27,7 @@ def get_context(srvr):
 
 def get_proxy(key='local'):
     srvr = SERVERS[key]
-    context = get_context(srvr) 
+    context = get_context(srvr)
     proxy = xmlrpclib.ServerProxy(srvr['url'], allow_none=True, context=context)
     return proxy
 
@@ -37,27 +37,23 @@ ind = Indicator(lots)
 
 remote_settings_key = 'local' if socket.gethostname() == 'picasso-kubuntu' else 'prime'
 
-for item in q:        
-    item_dict = item.get_item_dict()              
-    proxy = get_proxy(remote_settings_key)    
+for item in q:
+    item_dict = item.get_item_dict()
+    proxy = get_proxy(remote_settings_key)
     try:
         ind.update()
         result = proxy.add_lot(item_dict)
-        status = result.get('status')        
+        status = result.get('status')
         if status == SpiderItem.PROCESSED:
             item.estate_id = result['estate_id']
-            item.status = SpiderItem.PROCESSED    
-            logging.info("id: %s, processed estate_id: %s" % (item.id, item.estate_id))                    
+            item.status = SpiderItem.PROCESSED
+            logging.info("id: %s, processed estate_id: %s" % (item.id, item.estate_id))
         elif status is not None:
             item.status = status
-            logging.error("id: %s, %s" % (item.id, result['error_message']))        
+            logging.error("id: %s, %s" % (item.id, result['error_message']))
         else:
             logging.error('id: %s, result without status' % item.id)
         item.event_date = datetime.datetime.now()
         item.save()
     except Exception, e:
-        logging.error(str(e)) 
-        
-
-
-    
+        logging.error(str(e))
